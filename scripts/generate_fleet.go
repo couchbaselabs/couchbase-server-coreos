8ff7bb540efcd973edec2226a0ddffb989129177
Traun Leyden <tleyden@couchbase.com>
2014-12-03 09:38:13 -0800

    generate fleet (untested)

---
+++ scripts/generate_fleet.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"os"
+	"path"
+	"path/filepath"
+	"text/template"
+)
+
+type Params struct {
+	CB_VERSION string
+}
+
+func main() {
+
+	if len(os.Args) < 2 {
+		log.Fatal("Usage: ./generate_fleet <couchbase version> <dest dir>")
+		return
+	}
+
+	params := Params{
+		CB_VERSION: os.Args[1],
+	}
+
+	destDir := os.Args[2]
+
+	templateFiles := []string{
+		"../templates/fleet/couchbase_bootstrap_node.service",
+		"../templates/fleet/couchbase_node.service.template",
+	}
+
+	for _, templateFile := range templateFiles {
+
+		templateBytes, err := ioutil.ReadFile(templateFile)
+		if err != nil {
+			panic(err)
+		}
+
+		tmpl, err := template.New("docker").Parse(string(templateBytes))
+		if err != nil {
+			panic(err)
+		}
+
+		// create a writer that is going to write to <dest dir>/templateFile
+		filename := filepath.Split(templateFile)
+		destFile := path.Join(destDir, filename)
+
+		f, err := os.Create(destFile)
+		if err != nil {
+			panic(err)
+		}
+		defer f.Close()
+
+		// execute template and write to dest
+		err = tmpl.Execute(f, params)
+		if err != nil {
+			panic(err)
+		}
+
+	}
+
+}
