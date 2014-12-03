9470da64bee9ce5b8ad2eb984d568057baf5167f
Traun Leyden <tleyden@couchbase.com>
2014-12-03 10:07:23 -0800

    rename directory to 2.2.0

--- scripts/generate_fleet.sh
+++ scripts/generate_fleet.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-go run generate_fleet/generate_fleet.go 2.2.0 ../2.2/fleet
+go run generate_fleet/generate_fleet.go 2.2.0 ../2.2.0/fleet
 go run generate_fleet/generate_fleet.go 3.0.1 ../3.0.1/fleet
 
 cp ../templates/fleet/couchbase_bootstrap_node_announce.service \
    ../templates/fleet/create_node_services.sh \
-   ../2.2/fleet 
+   ../2.2.0/fleet 
 
 cp ../templates/fleet/couchbase_bootstrap_node_announce.service \
    ../templates/fleet/create_node_services.sh \
--- scripts/generate_scripts.sh
+++ scripts/generate_scripts.sh
@@ -2,5 +2,5 @@
 
 # For now, no templates needed, just copy ..
 
-cp ../scripts/* ../2.2/scripts
+cp ../scripts/* ../2.2.0/scripts
 cp ../scripts/* ../3.0.1/scripts
