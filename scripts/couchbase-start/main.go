8deda1fc5c2978b94990c60431ab15132f08712e
Traun Leyden <tleyden@couchbase.com>
2015-01-16 15:27:29 -0800

    experimenting w/ etcd

--- scripts/couchbase-start/main.go
+++ scripts/couchbase-start/main.go
@@ -2,92 +2,113 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/coreos/go-etcd/etcd"
 )
 
+const (
+	KEY_CLUSTER_INITIAL_NODE = "cluster-initial-node"
+	TTL_NONE                 = 0
+)
+
 func main() {
 
-	client := etcd.NewClient([]string{"foo"})
+	ip := "127.0.0.1"
+
+	client := etcd.NewClient([]string{"http://127.0.0.1:4001"})
+	client.SyncCluster()
+
+	nodes := client.GetCluster()
+
+	fmt.Println("nodes: %+v", nodes)
+
 	fmt.Println("client: %v", client)
+
+	response, err := client.Create(KEY_CLUSTER_INITIAL_NODE, ip, TTL_NONE)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("response: %+v", response)
 
 	/*
 
 
-	           # Discussion with James:
+		           # Discussion with James:
 
-	           # Use client.write('/cluster-first-node', '<our ip address>', prevExist=False) to
-	           # to do an ADD operation.  Only first one to do this will win.
+		           # Use client.write('/cluster-first-node', '<our ip address>', prevExist=False) to
+		           # to do an ADD operation.  Only first one to do this will win.
 
-	           # Each node will have a sidekick to maintain its state in
-	           # /node-state/<node ip> -> ON or the key will be missing, meaning its not running
+		           # Each node will have a sidekick to maintain its state in
+		           # /node-state/<node ip> -> ON or the key will be missing, meaning its not running
 
-	           # When a node comes up:
-	           #    - It will try to be the cluster-first-node
-	           #    - If succeeds:
-	           #       - couchbase-cli cluster-init
-	           #       - couchbase-cli bucket-create
-	           #    - Else: (there is already a cluster)
-	           #       - Find list of machines in cluster from etcd
-	           #       - Loop over machines
-	           #           - Get ip of machine
-	           #           - Does /node-state/<ip> key exist?
-	           #              - If it does, try to join the cluster via couchbase-cli server-add
-
-
+		           # When a node comes up:
+		           #    - It will try to be the cluster-first-node
+		           #    - If succeeds:
+		           #       - couchbase-cli cluster-init
+		           #       - couchbase-cli bucket-create
+		           #    - Else: (there is already a cluster)
+		           #       - Find list of machines in cluster from etcd
+		           #       - Loop over machines
+		           #           - Get ip of machine
+		           #           - Does /node-state/<ip> key exist?
+		           #              - If it does, try to join the cluster via couchbase-cli server-add
 
 
 
-		   # Scenarios
 
-		   # 1. Setting up cluster from scratch (recent machine boot)
-		   #
-		   #   1A. We become the leader
-		   #   1B. Someone beats us to becoming the leader, so we join the leader
-		   #
-		   # 2. This node rebooted and there is an existing cluster
-		   #
-		   #   2A. We *were* the leader previously
-		   #   2B. We were not the leader previously
 
-		   #  ---- v2
+			   # Scenarios
 
-		   # Call etcd to get cluster-leader-counter key
+			   # 1. Setting up cluster from scratch (recent machine boot)
+			   #
+			   #   1A. We become the leader
+			   #   1B. Someone beats us to becoming the leader, so we join the leader
+			   #
+			   # 2. This node rebooted and there is an existing cluster
+			   #
+			   #   2A. We *were* the leader previously
+			   #   2B. We were not the leader previously
 
-		       # If 0, then we are setting up cluster from scratch (recent machine boot)
+			   #  ---- v2
 
-		         # success = try_to_become_leader()
-		         # if success:
-		         #     cluster_one_time_init()
-		         #     create_bucket()
-		         # else:
-		         #     leader = find_leader() && join_leader(leader)
+			   # Call etcd to get cluster-leader-counter key
 
-		       # If >= 1, then this node rebooted and there is an existing cluster
+			       # If 0, then we are setting up cluster from scratch (recent machine boot)
 
-		         # leader = find_leader()
-		         # if leader == null: abort
-		         # join_leader(leader)
+			         # success = try_to_become_leader()
+			         # if success:
+			         #     cluster_one_time_init()
+			         #     create_bucket()
+			         # else:
+			         #     leader = find_leader() && join_leader(leader)
 
-		   # try_to_become_leader():
+			       # If >= 1, then this node rebooted and there is an existing cluster
 
-		      # Call etcd to get cluster-leader-ip key
+			         # leader = find_leader()
+			         # if leader == null: abort
+			         # join_leader(leader)
 
-		      # if non-empty, abort and return error
+			   # try_to_become_leader():
 
-		      # Use previous key index to do a CAS request to write our ip to cluster-leader-ip
+			      # Call etcd to get cluster-leader-ip key
 
-		      # if CAS request failed, abort and return error
+			      # if non-empty, abort and return error
 
-		      # otherwise, we are the leader
+			      # Use previous key index to do a CAS request to write our ip to cluster-leader-ip
 
-		   # find_leader()
+			      # if CAS request failed, abort and return error
 
-		     # Call etcd to get cluster-leader-ip key or return error
+			      # otherwise, we are the leader
 
-		   # join_leader()
+			   # find_leader()
 
-		     #
+			     # Call etcd to get cluster-leader-ip key or return error
+
+			   # join_leader()
+
+			     #
 
 
 
