63a7f45b25bafec52bb293dfd1a97f3cb679186b
Traun Leyden <tleyden@couchbase.com>
2015-01-19 09:10:03 -0800

    change to shell wrapper that downloads go program

--- scripts/generate_fleet.sh
+++ scripts/generate_fleet.sh
@@ -2,9 +2,3 @@
 
 go run generate_fleet/generate_fleet.go 2.2.0 ../2.2.0/fleet
 go run generate_fleet/generate_fleet.go 3.0.1 ../3.0.1/fleet
-
-cp ../templates/fleet/couchbase_announce@.service \
-   ../2.2.0/fleet 
-
-cp ../templates/fleet/couchbase_announce@.service \
-   ../3.0.1/fleet 
