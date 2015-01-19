d833b359a2d1e3856731666b466ef6d0950b6a39
Traun Leyden <tleyden@couchbase.com>
2015-01-18 21:18:27 -0800

    fixing fleet

--- scripts/generate_fleet.sh
+++ scripts/generate_fleet.sh
@@ -3,10 +3,4 @@
 go run generate_fleet/generate_fleet.go 2.2.0 ../2.2.0/fleet
 go run generate_fleet/generate_fleet.go 3.0.1 ../3.0.1/fleet
 
-cp ../templates/fleet/couchbase_bootstrap_node_announce.service \
-   ../templates/fleet/create_node_services.sh \
-   ../2.2.0/fleet 
 
-cp ../templates/fleet/couchbase_bootstrap_node_announce.service \
-   ../templates/fleet/create_node_services.sh \
-   ../3.0.1/fleet 
