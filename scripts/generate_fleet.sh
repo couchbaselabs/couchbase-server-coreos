b64b47e8e043322e8b359ccde295a796fc0d3a70
Traun Leyden <tleyden@couchbase.com>
2015-01-18 22:43:42 -0800

    add couchbase_node_announce

--- scripts/generate_fleet.sh
+++ scripts/generate_fleet.sh
@@ -3,4 +3,8 @@
 go run generate_fleet/generate_fleet.go 2.2.0 ../2.2.0/fleet
 go run generate_fleet/generate_fleet.go 3.0.1 ../3.0.1/fleet
 
+cp ../templates/fleet/couchbase_node_announce@.service \
+   ../2.2.0/fleet 
 
+cp ../templates/fleet/couchbase_node_announce@.service \
+   ../3.0.1/fleet 
